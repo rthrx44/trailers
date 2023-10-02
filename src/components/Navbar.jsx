6e0e2e72a1c6b4956d02fd4787f81d383b48a240
Ruther Dio <111130814+rthrx44@users.noreply.github.com>
2023-10-02 21:47:25 +0800

    fix navbar, try calling API and get pictures

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Trailers from './assets/Trailers.png' 
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+
 function Navbar () {
   return (
     <>
@@ -8,32 +9,59 @@ function Navbar () {
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* Page content here */}
-          <header className='flex justify-between items-center bg-black pl-4'>
+          <header className='container mx-auto flex justify-between items-center bg-[#141414] color-[#fff] pl-4 py-2 md:py-4'>
             <div>
-              <img src={Trailers} alt="Trailers Logo" className='h-5' />
+              <img src={Trailers} alt="Trailers Logo" className='h-5 xs:h-6 lg:h-7' />
             </div>
-            <div>
-              <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary bg-transparent border-none hover:bg-transparent active:text-red-600 md:hidden"><MenuRoundedIcon/></label>            
-              <ul className='flex gap-6'>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
+            <div className='flex items-center md:gap-6'>
+              <form className='tracking-widest relative'>
+                <input type="text" placeholder='SEARCH' className='pl-2 py-1 w-32 rounded-sm text-xs font-semibold outline-none focus:outline-1 focus:outline-red-700 transition-all ease-in-out lg:text-sm' />
+              </form>
+              <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary bg-transparent border-none outline-none hover:bg-transparent hover:text-red-700 active:text-red-700 md:hidden"><MenuOpenIcon/></label>            
+              <ul className='gap-6 text-xs tracking-widest hidden md:flex md:items-center md:pr-4 lg:text-sm'>
+                <li className='hover:text-red-700 cursor-pointer font-semibold transition-all ease-in-out'>HOME</li>
+                <li className='hover:text-red-700 cursor-pointer font-semibold transition-all ease-in-out'>MOVIES</li>
+                <li className='hover:text-red-700 cursor-pointer font-semibold transition-all ease-in-out'>TV SHOWS</li>
+                <li className='hover:text-red-700 cursor-pointer font-semibold transition-all ease-in-out'>WATCH LIST</li>
+                <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary bg-transparent px-0 border-none outline-none hover:bg-transparent hover:text-red-700 active:text-red-700"><MenuOpenIcon/></label>            
               </ul>
             </div>
           </header>
         </div> 
         <div className="drawer-side">
           <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-3/6 min-h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
+          <ul className="menu p-4 w-3/6 min-h-full text-base-content bg-[#141414] color-[#fff] gap-6">
             <div className='flex justify-center'>
-              <img src={Trailers} alt="Trailers Logo" className='h-5'/>
+              <img src={Trailers} alt="Trailers Logo" className='h-5 xs:h-6 lg:h-7'/>
             </div>
-            <ul>
-
-            </ul>
+            <div className='flex flex-col justify-center items-center text-xs gap-8 tracking-widest lg:text-sm'>
+              <li className='cursor-pointer hover:text-red-700 font-semibold md:hidden'>HOME</li>
+              <li className='cursor-pointer hover:text-red-700 font-semibold md:hidden'>MOVIES</li>
+              <li className='cursor-pointer hover:text-red-700 font-semibold md:hidden'>TV SHOWS</li>
+              <li className='cursor-pointer hover:text-red-700 font-semibold md:hidden'>WATCH LIST</li>
+              <li className='border-b border-[#fff] w-24 md:hidden'></li>
+              <h4 className='font-extrabold tracking-widest text-red-700'>GENRE</h4>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>ACTION</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>ADVENTURE</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>ANIMATION</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>COMEDY</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>CRIME</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>DOCUMENTARY</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>DRAMA</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>FAMILY</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>FANTASY</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>HISTORY</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>HORROR</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>MUSIC</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>MYSTERY</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>ROMANCE</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>SCIENCE FICTION</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>TV MOVIE</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>THRILLER</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>WAR</li>
+              <li className='cursor-pointer hover:text-red-700 md:flex'>WESTERN</li>
+            </div>
           </ul>
         </div>
       </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 
 function Landing() {
+  const [movies, setMovies] = useState([])
+
+  const fetchMovie = () => {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=2f1b97f3c5516282fbe25825bb55595c')
+    .then(res => res.json())
+    .then(json => setMovies(json.results))
+    .catch(err => console.error('error:' + err));
+  }
+
+  useEffect(() => {
+    fetchMovie()
+  })
+
   return (
     <div>
       <Navbar/>
+      <section>
+        <div>
+          {movies.map((movie, idx) => (
+            <div key={idx}>
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.id}/>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   )
 }
